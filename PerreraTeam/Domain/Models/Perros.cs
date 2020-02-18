f1f40c174e267fc8e66697c17e5143d5353fca02
AndreaSil <andreasilas.as@gmail.com>
2020-02-18 13:53:31 +0100

    se han creado los modelos de Jaulas, Perros y Razas

---
+++ PerreraTeam/Domain/Models/Perros.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PerreraTeam.Domain.Models
+{
+    public class Perros
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ha de indicar un nombre para el perro")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Indique el Chip del perro")]
+        public string Chip { get; set; }
+
+        [DataType(DataType.Date)]
+        public Nullable<System.DateTime> FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "Código de raza del perro")]
+        public Nullable<int> CodRazaId { get; set; }
+
+        [Required(ErrorMessage = "Código de jaula")]
+        public Nullable<int> IdJaula { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<Adopciones> Adopciones { get; set; }
+        public virtual Jaulas Jaulas { get; set; }
+        public virtual Razas Razas { get; set; }
+    }
+}
