0272899c47a210216c9cdb849013094a8be30ebc
Alejandro Gaspar <AlejandroWork97@hotmail.com>
2020-02-18 13:49:48 +0100

    ViewModelsBase, Adopcion,Clientes,Empleados

---
+++ PerreraTeam/ViewModels/AdopcionesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PerreraTeam.ViewModels
+{
+    public class AdopcionesViewModel
+    {
+        public int PerroId { get; set; }
+        public int ClienteId { get; set; }
+        public int EmpleadoId { get; set; }
+        [Required(ErrorMessage = "Se requiere una fecha de entrega correcta")]
+        public System.DateTime FechaEntrega { get; set; }
+
+        //public virtual Clientes Clientes { get; set; }
+       // public virtual Empleados Empleados { get; set; }
+       // public virtual Perros Perros { get; set; }
+    }
+}
