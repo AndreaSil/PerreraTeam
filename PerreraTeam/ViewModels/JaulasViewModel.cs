155aae1390a33e9adbe9965aee43700f7af794d1
Sermocho <sermocho@yahoo.es>
2020-02-19 09:21:17 +0100

    ViewModels de Perros, Razas y Jaula

---
+++ PerreraTeam/ViewModels/JaulasViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PerreraTeam.Domain.Models;
+
+namespace PerreraTeam.ViewModels
+{
+    public class JaulasViewModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ha de indicar un nombre para la jaula")]
+        public string NombreJaula { get; set; }
+
+        public virtual ICollection<Perros> Perros { get; set; }
+    }
+}
