096ae8c6bf1d389a49a229ceb4e6f5673e4281ab
AndreaSil <andreasilas.as@gmail.com>
2020-02-19 12:48:37 +0100

    Automapper

---
+++ PerreraTeam/AutoMapper/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using AutoMapper;
+using PerreraTeam.Domain.Models;
+using PerreraTeam.ViewModels;
+
+namespace PerreraTeam.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+
+        public static MapperConfiguration InitializeAutoMapper()
+        {
+            MapperConfiguration config = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.CreateMap<Perros, PerrosViewModel>();
+                    cfg.CreateMap<Jaulas, JaulasViewModel>();
+                    cfg.CreateMap<Adopciones, AdopcionesViewModel>();
+                    cfg.CreateMap<Empleados, EmpleadosViewModel>();
+                    cfg.CreateMap<Clientes, ClientesViewModel>();
+                    cfg.CreateMap<Razas, RazasViewModel>();
+                });
+            return config;
+        }
+
+
+
+    }
+}
